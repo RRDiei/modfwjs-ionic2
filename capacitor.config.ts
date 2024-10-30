29c2fc8fcc5a8c1c72d78cc0fa7508f2889680ff
Diego García Esteban <diegoges@protonmail.com>
2024-10-30 18:00:53 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'modfwjs-ionic2',
+  webDir: 'dist'
+};
+
+export default config;
